089b9b3d34a8c05386b5e0719b1bba97dcb42e50
designUon <design_uon@naver.com>
2023-03-09 18:15:14 +0900

    0309 함수와 변수

---
+++ js/변수의 적용범위.js
@@ -0,0 +1,10 @@
+document.write(`<p><b>script src : 외부스크립트</b></p>`);
+
+let name = "홍길동";
+document.write(`<p>${name}</p>`);
+document.write(`<p>${name}</p>`);
+
+var bookTitle = "책제목";
+document.write(`${bookTitle}`);
+
+document.write(`<hr>`);
